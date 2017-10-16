a05ee4f6634d76ad043e7c11e4927faa736e9481
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-10-16 11:48:16 +0300

    server done without extentions

--- beerModel.js
+++ beerModel.js
@@ -1,1 +1,15 @@
+// [{ name: '512 IPA', style: 'IPA', image_url: 'http://bit.ly/1XtmB4d', abv: 5 },
+// { name: '512 Pecan Porter', style: 'Porter', image_url: 'http://bit.ly/1Vk5xj4', abv: 4 }]
+var mongoose = require('mongoose');
 
+var beerSchema = new mongoose.Schema({
+    name: { type: String },
+    style: { type: String },
+    image_url: { type: String },
+    abv: { type: Number },
+    ratings: [Number]
+  });
+
+var Beer = mongoose.model("Beer", beerSchema);
+
+module.exports = Beer;
