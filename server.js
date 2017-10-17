2f0678174c242839c8e64bdafadea9af7dba13cc
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-10-17 15:03:19 +0300

    client without extensions

--- server.js
+++ server.js
@@ -12,39 +12,20 @@ app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/beers', function(request, response) {
-  Beer.find({}, function(err, res) {
-    if (err) {
-      return next(err);
-    } else {
-      return response.send(res);
-    }
-  });
+app.get('/beers', function (request, response, next) {
+  Beer.find({}, handler(response, next));
 });
 
 app.post('/beers', function (request, response, next) {
-  Beer.create(request.body, function(err, res) {
-    if (err) {
-      return next(err);
-    } else {
-      return response.send(res);
-    }
-  });
+  Beer.create(request.body, handler(response, next));
 });
 
-app.delete('/beers/:id', function(request, response, next) {
-  console.log(request.params.id);
-  Beer.findByIdAndRemove(request.params.id, function(err, res){
-    if (err) {
-      return next(err);
-    } else {
-      return response.send(res);
-    }
-  });
+app.delete('/beers/:id', function (request, response, next) {
+  Beer.findByIdAndRemove(request.params.id, handler(response, next));
 });
 
 // error handler to catch 404 and forward to main error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -52,7 +33,7 @@ app.use(function(req, res, next) {
 
 // main error handler
 // warning - not for use in production code!
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.send({
     message: err.message,
@@ -60,6 +41,18 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(8000, function() {
+app.listen(8000, function () {
   console.log("yo yo yo, on 8000!!");
 });
+
+
+// FUNCTIONS //
+function handler(response, next) {
+  return function (err, res) {
+    if (err) {
+      return next(err);
+    } else {
+      return response.send(res);
+    }
+  };
+}
